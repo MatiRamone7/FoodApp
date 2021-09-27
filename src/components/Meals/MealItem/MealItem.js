d110c0735416490cd69a0be1730b5c12e1afd35f
Matias <mfida@quadiontech.com>
2021-09-27 11:53:10 -0300

    Items y logica

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
+import { useContext } from 'react';
+import CartContext from '../../../store/cart-context';
 
 function MealItem(props) {
-    const price = `$${props.price.toFixed(2)}`
+    const cartCtx = useContext(CartContext);
+
+    const price = `$${props.price.toFixed(2)}`;
+
+    const addToCartHandler = amount => {
+        cartCtx.addItem({
+            id : props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        });
+    };
 
     return (
         <li className={classes.meal}>
@@ -13,7 +27,7 @@ function MealItem(props) {
             </div>
 
             <div>
-
+                <MealItemForm onAddToCart={addToCartHandler} />
             </div>
         </li>
     )
